0dbfefa4c22ba2d170f31a334779251a56c7c64d
john morris <johncmorris@gmail.com>
2020-09-12 07:46:08 -0700

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Target;
+    
+    public enum FollowTypes
+    {
+        Offset,
+        Smooth,
+        Rotation,
+        SmoothRotation
+    }
+
+    public FollowTypes FollowType;
+    public float Smoothing = 1;
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Target.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(FollowType == FollowTypes.Offset)
+        {
+            transform.position = Target.position + offset;
+        }else if (FollowType == FollowTypes.Smooth)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.position + offset, Smoothing * Time.fixedTime);
+        }
+        else if (FollowType == FollowTypes.Rotation)
+        {
+            transform.position = Target.position + offset;
+            Vector3 direction = Target.GetComponent<Rigidbody>().velocity;
+            direction = new Vector3(direction.x, 0, direction.z);
+            if(direction.magnitude > 0)
+            {
+
+            }
+        }
+        else if (FollowType == FollowTypes.SmoothRotation)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.position + offset, Smoothing * Time.fixedTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Power = 5;
+    public enum ControlTypes
+    {
+        velocity,
+        force,
+        acceleration
+    }
+    public ControlTypes ControlType;
+    private Rigidbody rb;
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handlePlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        if(ControlType == ControlTypes.velocity)
+        {
+            rb.velocity = movement;
+        }else if (ControlType == ControlTypes.force)
+        {
+            rb.AddForce(movement);
+        }
+        else if (ControlType == ControlTypes.acceleration)
+        {
+
+        }
+    }
+
+    void handlePlayerInput()
+    {
+        float h = Input.GetAxis("Horizontal") * Power;
+        float v = Input.GetAxis("Vertical") * Power;
+        float n = 0;
+        if (ControlType == ControlTypes.velocity) n = rb.velocity.y;
+
+        movement = new Vector3(h, n, v);
+    }
+}
